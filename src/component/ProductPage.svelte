f26ae345da0235c0b3d9c19749f4228e2cce0b7f
Nilson Torres <nilson@vertece.com>
2025-12-25 02:40:12 -0400

    25-12-2025-02-40

--- src/component/ProductPage.svelte
+++ src/component/ProductPage.svelte
@@ -1,8 +1,14 @@
 <script>
     let { changePage=()=>{}, locked=false } = $props();
+
+    let scrollable = $state(null);
+
+    export const scrollTo = (params) => {
+        scrollable.scrollTo(params);
+    }
 </script>
 
-<div class="flex flex-col w-full min-h-dvh justify-center items-center bg-amber-500 relative">
+<div bind:this={scrollable} class={`flex flex-col w-full min-h-dvh justify-center items-center bg-amber-500 relative ${locked ? "overflow-y-scroll" : "overflow-hidden"}`} >
     <span class="text-black text-[0.875rem]">Página de produto</span>
     <button class="text-black font-semibold mt-5" type="button" onclick={() => changePage("add_shipping")}>Adicionar endereço</button>
 </div>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -2,9 +2,10 @@
     import PageTransition from "$component/PageTransition.svelte";
     import FinalizationPage from "$component/FinalizationPage.svelte";
     import AddShippingPage from "$component/AddShippingPage.svelte";
+    import ProductPage from "$component/ProductPage.svelte";
 </script>
 
 <PageTransition pages={[
-    {name: "finalization", color: "#FFFFFF", component: FinalizationPage, props: {id: "ccf7dbe7-bd3a-4b56-8e1e-df51b9e67100", images: []}},
+    {name: "finalization", color: "#FFFFFF", component: ProductPage, props: {id: "ccf7dbe7-bd3a-4b56-8e1e-df51b9e67100", images: []}},
     {name: "add_shipping", color: "#F5F5F5", component: AddShippingPage}
 ]}/>
