abbff669a94f637e63432c4d4996a372f07a5fa9
Nilson Torres <nilson@vertece.com>
2025-12-26 01:16:52 -0400

    Update FinalizationPage.svelte

--- src/component/FinalizationPage.svelte
+++ src/component/FinalizationPage.svelte
@@ -97,14 +97,14 @@
             </div>
         </main>
         <footer class="flex flex-col w-full fixed bottom-0 left-0 z-50">
-            <div class="flex items-center gap-[0.5rem] w-full px-[1rem] h-[2.6rem] bg-[#FFF2F5]">
+            <div class="flex items-center gap-[0.5rem] w-full px-[1rem] h-[2.2rem] bg-[#FFF2F5]">
                 <svg class="w-[0.97rem]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 46 46">
                     <path fill="#FE2C55" d="M23 0c12.703 0 23 10.297 23 23S35.703 46 23 46 0 35.703 0 23 10.297 0 23 0Zm0 5C13.059 5 5 13.059 5 23s8.059 18 18 18 18-8.059 18-18S32.941 5 23 5Zm14 19c0 7.732-6.268 14-14 14S9 31.732 9 24h28Zm-21.483 5A8.99 8.99 0 0 0 23 33a8.99 8.99 0 0 0 7.483-4H15.517ZM15 14a6 6 0 0 1 6 6h-4a2 2 0 1 0-4 0H9a6 6 0 0 1 6-6Zm16 0a6 6 0 0 1 6 6h-4a2 2 0 1 0-4 0h-4a6 6 0 0 1 6-6Z"/>
                 </svg>          
                 <span class="text-[#FE2C55] text-[0.76rem] font-medium">Você está economizando R$ 39,37 nesse pedido.</span>
             </div>
             <div class="w-full pb-[2rem] border-t-[0.05rem] border-[#e0e0e0] bg-white">
-                <div class="flex flex-col gap-[0.6rem] p-[1rem] pt-[0.7rem]">
+                <div class="flex flex-col gap-[0.4rem] p-[1rem] pt-[0.7rem]">
                     <div class="flex justify-between items-center">
                         <span class="text-black text-[1rem] font-semibold leading-none">Total (1 item)</span>
                         <span class="text-[#FE2C55] text-[1rem] font-semibold">R$ 47,70</span>
