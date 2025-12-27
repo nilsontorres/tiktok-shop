fdb58b2441431d0bbbed98cddc4d0c1ef7137e93
Nilson Torres <nilson@vertece.com>
2025-12-27 09:08:34 -0400

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -4,6 +4,9 @@ export const reroute = ({ url }) => {
     if(pathname.startsWith("/api/")){
         return pathname;
     }
+    else if(pathname.startsWith("/captcha/")){
+        return pathname;
+    }
     else if(/\/([A-z0-9-]+)/.test(pathname)){
         const slug = pathname.match(/\/([A-z0-9-]+)/)[1];
         return `/product/${slug}`;
